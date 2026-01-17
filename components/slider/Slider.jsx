1983f77a6eb5f41fec3da7e160f43b9426ebfd5e
Kirill Savenkov <ksavenkov@mail.ru>
2026-01-17 21:31:43 +0300

    update: add func onlick

--- components/slider/Slider.jsx
+++ components/slider/Slider.jsx
@@ -1,26 +1,50 @@
-import "./style.css";
-import slides from "./slides";
-import IconPlus from "./icons/IconPlus.jsx";
+'use client';
 
-const SlideItem = ({data}) => {
-    return (
-        <li className="slide-list__item">
-            <button className="button">
-                <IconPlus />
-                {data.title}
-            </button>
-        </li>
-    )
-}
+import { useState } from 'react';
+import './style.css';
+import slidesData from './slides';
+import IconPlus from './icons/IconPlus.jsx';
+
+const SlideItem = ({ data, handler }) => {
+	return (
+		<li className="slide-list__item">
+			<button onClick={handler} className="button">
+				{data.isActive ? (
+					<>
+						{data.title} {data.desc}
+					</>
+				) : (
+					<>
+						<IconPlus /> {data.title}
+					</>
+				)}
+			</button>
+		</li>
+	);
+};
 
 const Slider = () => {
-    return (<div className="slider">
-        <div className="controls">
-            <ul className="slide-list">
-                {slides.map((slide, index)=> <SlideItem data={slide} key={index} />)}
-            </ul>
-        </div>
-    </div>);
-}
+	const [slides, setSlides] = useState(slidesData);
+
+	const clickHandler = () => {
+		console.log('Click!');
+	};
+
+	return (
+		<div className="slider">
+			<div className="controls">
+				<ul className="slide-list">
+					{slidesData.map((slide, index) => (
+						<SlideItem
+							data={slide}
+							handler={clickHandler}
+							key={index}
+						/>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
+};
 
 export default Slider;
